01156ee638861bdd8e9b134fa658fc0dbbd2570a
deepanshu102 <deepanshujindal1021997@gmail.com>
2019-03-26 23:55:15 +0530

    Native String Matching Algo1

---
+++ 12_Naive.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<limits>
+#include<vector>
+using namespace std;
+
+class Native
+{
+	private:
+		string t,p;
+	public: Native()
+		{
+			input();
+		}
+		Native(string t,string p )
+		{
+			this->t=t;
+			this->p=p;
+		}
+		void input()
+		{
+
+			cout<<"Enter your Text:- ";
+			cin>>t;
+			fflush(stdin);
+			cout<<"\nEnter your Pattern:- ";
+			cin>>p;
+		}
+		int search()
+		{
+			int shift=0,n=t.length(),m=p.length();
+			int count=0,j;
+			for(shift=0;shift<=n-m;shift++)
+			{
+				count=0;
+				for(j=0;j<m;j++)
+				{
+					if(t[shift+j]==p[j])
+						count++;
+					else
+						break;
+				}
+				if(j==m & count==m)
+				{
+					break;
+				}
+			}
+			if(count==m)
+			{
+				cout<<"start:-"<<shift<<"\tend:-"<<shift+m;
+			}		
+			return 0;
+		}
+		void test()
+		{
+			int i,j;
+			cin>>i>>j;
+			for(;i<j;i++)
+			{
+				cout<<t[i];
+			}
+		}
+};
+int main()
+{
+	Native obj;
+	obj.search();
+	obj.test();
+}
+
